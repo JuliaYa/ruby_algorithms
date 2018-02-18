873f91333d8cbda14c93301bf1e42469d9872a90
JuliaYa <julia.yakutova@gmail.com>
2018-02-18 22:41:58 +0400

    Binary search.

---
+++ algorithms/binary_search.rb
@@ -0,0 +1,25 @@
+class Search
+  def binary(list, element)
+    low = 0
+    high = list.length - 1
+
+    while low < high
+      mid = (low + high) / 2
+      guess = list[mid]
+      return mid if guess == element
+      if guess > element
+        high = mid - 1
+      else
+        low = mid + 1
+      end
+    end
+
+    nil
+  end
+end
+
+list = [1, 3, 5, 7, 9, 12, 15]
+search = Search.new
+puts search.binary(list, 3)
+puts search.binary(list, 12)
+puts search.binary(list, 36)

2c7a017598ba5a3eca00f34bf66612bd7875dbb6
JuliaYa <julia.yakutova@gmail.com>
2018-02-18 23:54:41 +0400

    Max element from array.

--- algorithms/array_sum.rb
+++ algorithms/array_sum.rb
@@ -1,10 +1,19 @@
-class Sum
+class MyArray
   def sum(array)
     return 0 if array.empty?
     array.first + sum(array[1..-1])
   end
+
+  def max(array)
+    return array.first if array.size == 1
+    max_tail = max(array[1..-1])
+    array.first >= max_tail ? array.first : max_tail
+  end
 end
 
 list = [1, 3, 5, 7, 9, 12, 15]
-summy = Sum.new
+list2 = [4, 1, 6, 12, 35, 3, 0, 35]
+summy = MyArray.new
 puts summy.sum list
+puts summy.max list
+puts summy.max list2
